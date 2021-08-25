4a7f80c90c2fdef558d51fa61243f059439b7465
Linus Willner <hello@linuswillner.me>
2021-08-25 12:54:24 +0300

    Fix Lombok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import lombok.Getter;
+
 public class Main {
+  @Getter
+  private static final String test = "test";
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.printf("Hello World, this is a %s", Main.getTest());
   }
 }
